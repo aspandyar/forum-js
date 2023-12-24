bbe2bc640a5f1d24db8e5be2adba7306c6803a50
aspandyar <aspandyart@gmail.com>
2023-12-24 19:14:31 +0600

    adding css and html

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-let num = 2
-console.log(`Var: ${num}`)
+const express = require('express')
+const ejs = require('ejs')
+
+const PORT = 3000
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/user/:username', (req, res) => {
+    let data = {username: req.params.username, abouts: ['Football', 'Basketball', 'Vollewbal']}
+    res.render('user', data)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started: http://localhost:${PORT}`)
+})
