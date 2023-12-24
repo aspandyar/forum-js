11fe51bcb475966c536910f32d47c8a626723c28
aspandyar <aspandyart@gmail.com>
2023-12-24 22:05:15 +0600

    some basic pages

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 init
+nodemon => to run server and do upgrade in real time
+express => npm for eqsiest way to work with node routes
+ejs => allow to work with html/css files by creating js logic inside that files, and other
+app.use((req, res, next) => {next();}); => middleware, work in that row, how it written, and if we add this after any route it would not run after it
--- index.js
+++ index.js
@@ -1,26 +1,43 @@
-const express = require('express')
-const ejs = require('ejs')
+const express = require('express');
+const mongoose = require('mongoose');
 
-const PORT = 3000
+const PORT = 3000;
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+// view engine
+app.set('view engine', 'ejs');
 
+// middleware
+app.use(express.urlencoded({ extended: false })); // get data from form
+app.use(express.static('public'));
+
+// database and server connection
+const DBuri = "mongodb+srv://aspandyar:AkUE6zyIGRZjtWNg@cluster0.fsit9t7.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(DBuri)
+  .then((result) => app.listen(PORT))
+  .catch((err) => console.log(err));
+
+
+// routes
 app.get('/', (req, res) => {
-    res.render('index')
-})
+    res.render('index');
+});
 
 app.get('/about', (req, res) => {
-    res.render('about')
-})
+    res.render('about');
+});
 
 app.get('/user/:username', (req, res) => {
-    let data = {username: req.params.username, abouts: ['Football', 'Basketball', 'Vollewbal']}
-    res.render('user', data)
-})
+    let data = { username: req.params.username, abouts: ['Football', 'Basketball', 'Vollewbal'] };
+    res.render('user', data);
+});
 
-app.listen(PORT, () => {
-    console.log(`Server started: http://localhost:${PORT}`)
-})
+app.post('/check-user', (req, res) => {
+    let username = req.body.username;
+    if (username == "")
+        return res.redirect('/');
+    else
+        return res.redirect('/user/' + username);
+});
